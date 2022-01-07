7345daf8633d653a206d4db1265cdec1363b127a
pikesley <sam.pikesley@gmail.com>
2022-01-07 17:25:54 +0000

    New colour tooling

--- lib/tools.py
+++ lib/tools.py
@@ -1,4 +1,14 @@
-def serial_ouput_to_grb(binary):
-    """Turn the serial output into a GRB triple."""
-    rgb = list(map(int, binary[1:-1].split(", ")))
+from colorsys import hsv_to_rgb
+
+from lib.gamma import gamma
+
+
+def hue_to_grb(hue):
+    """Test it generates a GRB triple from a hue."""
+    rgb = list(map(lambda x: int(x * 255), hsv_to_rgb(hue, 1, 1)))
     return [rgb[1], rgb[0], rgb[2]]
+
+
+def gamma_correct(triple):
+    """Gamma-correct a colour."""
+    return list(map(lambda n: gamma[n], triple))
--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,6 +1,32 @@
-from lib.tools import serial_ouput_to_grb
+from lib.tools import gamma_correct, hue_to_grb, serial_ouput_to_grb
 
 
 def test_serial_ouput_to_grb():
     """Test it parses the string correctly."""
     assert (serial_ouput_to_grb("(255, 19, 123)")) == [19, 255, 123]
+
+
+def test_hue_to_grb():
+    """Test it turns a `hue` value into a GRB triple."""
+    cases = (
+        (1, [0, 255, 0]),
+        (0.5, [255, 0, 255]),
+    )
+
+    for hue, grb in cases:
+        assert hue_to_grb(hue) == grb
+
+
+def test_gamma_correction():
+    """Test it gamma-corrects correctly."""
+    cases = (
+        ([0, 0, 0], [0, 0, 0]),
+        ([255, 255, 255], [255, 255, 255]),
+        ([255, 0, 255], [255, 0, 255]),
+        ([12, 34, 56], [0, 1, 4]),
+        ([112, 134, 156], [25, 42, 64]),
+        ([250, 251, 252], [241, 244, 247]),
+    )
+
+    for colour, corrected in cases:
+        assert gamma_correct(colour) == corrected
