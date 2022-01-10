522013647c41482b321eb86b483c8fecd7557351
pikesley <sam.pikesley@gmail.com>
2022-01-10 22:40:37 +0000

    Nuke unused method

--- lib/tools.py
+++ lib/tools.py
@@ -1,4 +1,4 @@
-from colorsys import hsv_to_rgb, rgb_to_hsv
+from colorsys import hsv_to_rgb
 
 from lib.gamma import gamma
 
@@ -7,12 +7,6 @@ def hue_to_grb(hue):
     """Generate a GRB triple from a hue."""
     rgb = list(map(lambda x: int(x * 255), hsv_to_rgb(hue, 1, 1)))
     return [rgb[1], rgb[0], rgb[2]]
-
-
-def grb_to_hue(grb):
-    """Convert a GRB triple to a hue value."""
-    rgb = [grb[1], grb[0], grb[2]]
-    return rgb_to_hsv(*list(map(lambda x: x / 255, rgb)))[0]
 
 
 def gamma_correct(triple):
--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,4 +1,4 @@
-from lib.tools import gamma_correct, grb_to_hue, hue_to_grb
+from lib.tools import gamma_correct, hue_to_grb
 
 
 def test_hue_to_grb():
@@ -10,17 +10,6 @@ def test_hue_to_grb():
 
     for hue, grb in cases:
         assert hue_to_grb(hue) == grb
-
-
-def test_grb_to_hue():
-    """Test it generates a `hue` from a GRB triple."""
-    cases = (
-        ([0, 255, 0], 0),
-        ([255, 0, 255], 0.5),
-    )
-
-    for grb, hue in cases:
-        assert grb_to_hue(grb) == hue
 
 
 def test_gamma_correction():
