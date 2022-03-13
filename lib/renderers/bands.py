54fdd6b92047c656323c298edcaf84a8c55295fc
pikesley <sam.pikesley@gmail.com>
2022-03-13 16:08:52 +0000

    This is baffling
    I think I'm counting from the wrong end

--- lib/renderers/bands.py
+++ lib/renderers/bands.py
@@ -1,38 +1,47 @@
 import pickle
 from pathlib import Path
-import numpy as np
+
+
 from lib.pixel_hat import PixelHat
 
 
-# THIS SHOULD WRITE JUST THE INDECES
 class Band(list):
     """Individual Band sequence."""
 
-    def __init__(self, width, count, axis, direction, hat=None):
+    def __init__(self, width, count, axis, direction, steps=200, hat=None):
         """Construct."""
         self.width = width
         self.count = count
         self.axis = axis
         self.direction = direction
+        self.steps = steps
 
         if hat:
             self.hat = hat
         else:
             self.hat = PixelHat(auto_centre=True)
-
-        self.append([])
-
-        for number in np.arange(-1, 2, 2 / (len(hat) - 1)):
+            
+        for number in get_intervals(self.steps):
             upper_bound = number
             lower_bound = number - self.width
+            filtered = filter(lambda w: lower_bound < w[axis] <= upper_bound, self.hat)
 
-            uppered = filter(lambda w: w[axis] <= upper_bound, self.hat)
-            lowered = filter(lambda w: w[axis] > lower_bound, uppered)
+            self.append(list(map(lambda w: w["index"], filtered)))
 
-            self.append(list(map(lambda w: w["index"], lowered)))
 
-        self.append([])
+def get_intervals(steps):
+    """Generate some intervals."""
+    intervals = [-1]
+    seed = -1
+    gap = 2 / (steps - 1)
+    while True:
+        seed += gap
+        if seed > 1:
+            return intervals
 
+        intervals.append(round(seed, 3))
+
+    # return intervals
 
 #     def render(self):
 #         """Create the data."""
--- tests/renderers/test_bands.py
+++ tests/renderers/test_bands.py
@@ -1,7 +1,8 @@
 from unittest import TestCase
 
+
 from lib.pixel_hat import PixelHat
-from lib.renderers.bands import Band
+from lib.renderers.bands import Band, get_intervals
 
 
 class TestBand(TestCase):
@@ -12,15 +13,30 @@ class TestBand(TestCase):
         hat = PixelHat(
             locations="tests/fixtures/bands/simplest-locations.yaml", auto_centre=True
         )
-        band = Band(1, 1, "x", "up", hat)
+        band = Band(1, 1, "x", "up", steps=2, hat=hat)
 
-        self.assertEqual(band, [[], [0], [1], []])
+        self.assertEqual(band, [[0], [1]])
 
     def test_three_location_rendering(self):
         """Test it renders with three simple locations."""
         hat = PixelHat(
             locations="tests/fixtures/bands/three-locations.yaml", auto_centre=True
         )
-        band = Band(1, 1, "x", "up", hat)
+        band = Band(1, 1, "x", "up", steps=3, hat=hat)
 
-        self.assertEqual(band, [[], [0], [1], [2], []])
+        self.assertEqual(band, [[0], [1], [2]])
+
+    def test_wider_rendering(self):
+        """Test it renders a wider band."""
+        hat = PixelHat(
+            locations="tests/fixtures/bands/simplest-locations.yaml", auto_centre=True
+        )
+        band = Band(2, 1, "x", "up", steps=3, hat=hat)
+
+        self.assertEqual(band, [[0], [0, 1], [1]])
+
+def test_get_intervals():
+    """Test the interval-getter."""
+    assert get_intervals(2) == [-1, 1]
+    assert get_intervals(3) == [-1, 0, 1]
+    assert get_intervals(4) == [-1, -0.333, 0.333, 1]
