93f5e76b23fa99fd2b9b3bcb754492ef1ceab535
pikesley <sam.pikesley@gmail.com>
2022-03-20 15:48:05 +0000

    OK this is good

--- lib/modes/bands.py
+++ lib/modes/bands.py
@@ -1,7 +1,6 @@
 from math import ceil
 from random import random
 from lib.tools import hue_to_rgb
-from lib.conf import conf
 from lib.mode import Mode
 
 
@@ -18,12 +17,12 @@ class Bands(Mode):
             self.hat.reverse()
 
         self.jump = self.conf["modes"]["bands"]["jump"]
+        self.width = self.conf["modes"]["bands"]["width"]
 
     def run(self):
         """Do the stuff."""
         colours_index = 0
         while True:
-            # colour = self.redisman.get_colour()
             colour = hue_to_rgb(random())
             for i in range(ceil(len(self.hat) / self.jump)):
                 for j in range(self.jump):
@@ -34,17 +33,17 @@ class Bands(Mode):
                             auto_show=False,
                         )
                     except IndexError:
-                        pass
-                self.hat.show()
+                        pass 
 
-                # try:
-                #     self.hat.light_one(
-                #     self.hat[i * self.jump + j - 10]["index"],
-                #         [0, 0, 0],
-                #         auto_show=False,
-                #     )
-                #     self.hat.show()
-                # except IndexError:
-                #     pass 
+                    try:
+                        self.hat.light_one(
+                        self.hat[i * self.jump + j - self.width]["index"],
+                            [0, 0, 0],
+                            auto_show=False,
+                        )
+                    except IndexError:
+                        pass
+
+                self.hat.show()
 
             colours_index = (colours_index + 1) % 6
