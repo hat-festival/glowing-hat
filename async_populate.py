bda8df9a9763458284b8e905c8960d6830105a43
pikesley <sam.pikesley@demandlogic.co.uk>
2024-02-09 22:41:40 +0000

    Tweak to work on the hat
    This is going to be hard to test

--- async_populate.py
+++ async_populate.py
@@ -4,26 +4,31 @@ from time import sleep
 
 from lib.custodian import Custodian
 
-cust = Custodian("test")
+cust = Custodian(namespace="hat")
 
 
 def pause():
-    sleep(0.3 + random() / 10)
+    sleep(0.2)
 
 
 while True:
-    cust.set("low", datetime.datetime.now().timestamp())
-    pause()
-    pause()
-    cust.set("high", datetime.datetime.now().timestamp())
-    pause()
     cust.set("mid", datetime.datetime.now().timestamp())
     pause()
-    cust.set("low", datetime.datetime.now().timestamp())
     pause()
-    cust.set("low", datetime.datetime.now().timestamp())
+    pause()
     pause()
     cust.set("high", datetime.datetime.now().timestamp())
     pause()
+    pause()
+    pause()
+    pause()
     cust.set("low", datetime.datetime.now().timestamp())
+    pause()
+    pause()
+    pause()
+    pause()
+    cust.set("high", datetime.datetime.now().timestamp())
+    pause()
+    pause()
+    pause()
     pause()
