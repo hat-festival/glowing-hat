2c22911dc809227d09f1d0689b9f276208dfc77f
pikesley <sam.pikesley@demandlogic.co.uk>
2024-02-09 23:00:36 +0000

    Working FFT scripts

---
+++ fft/src/fft.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+def getFFT(data, rate, chunk_size, log_scale=False):
+    data = data * np.hamming(len(data))
+    try:
+        FFT = np.abs(np.fft.rfft(data)[1:])
+    except:
+        FFT = np.fft.fft(data)
+        left, right = np.split(np.abs(FFT), 2)
+        FFT = np.add(left, right[::-1])
+
+    if log_scale:
+        try:
+            FFT = np.multiply(20, np.log10(FFT))
+        except Exception as e:
+            print("Log(FFT) failed: %s" % str(e))
+
+    return FFT
