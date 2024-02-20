ccb2bc02d534ff01554893e57a1951bd739eea2e
pikesley <sam.pikesley@demandlogic.co.uk>
2024-02-20 22:45:35 +0000

    Make the tests pass

--- lib/colour_scalers/rotary_encoder.py
+++ lib/colour_scalers/rotary_encoder.py
@@ -1,9 +1,11 @@
 from multiprocessing import Value
 from time import sleep
 
-from RPi import GPIO
-
 from lib.conf import conf
+from lib.tools import is_pi
+
+if is_pi():  # nocov
+    from RPi import GPIO
 
 
 class RotatingScaler:
@@ -20,44 +22,46 @@ class RotatingScaler:
         self.dt = self.conf["pins"]["dt"]
         self.sw = self.conf["pins"]["sw"]
 
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        GPIO.setup(self.dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        GPIO.setup(self.sw, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        if is_pi():
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setup(self.clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+            GPIO.setup(self.dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+            GPIO.setup(self.sw, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
     def run(self):
         """Do the work."""
-        clk_last_state = GPIO.input(self.clk)
-        sw_last_state = GPIO.input(self.sw)
-        on = True
+        if is_pi():
+            clk_last_state = GPIO.input(self.clk)
+            sw_last_state = GPIO.input(self.sw)
+            on = True
 
-        while True:
-            clk_state = GPIO.input(self.clk)
-            dt_state = GPIO.input(self.dt)
+            while True:
+                clk_state = GPIO.input(self.clk)
+                dt_state = GPIO.input(self.dt)
 
-            if clk_state != clk_last_state:
-                if dt_state != clk_state:
-                    self.normaliser.factor.value = min(
-                        self.normaliser.factor.value + self.step_size,
-                        self.max_brightness,
-                    )
-                else:
-                    self.normaliser.factor.value = max(
-                        self.normaliser.factor.value - self.step_size, 0.005
-                    )
-            clk_last_state = clk_state
-
-            new_sw_state = GPIO.input(self.sw)
-            if new_sw_state != sw_last_state:
-                if new_sw_state == 0:
-                    if on:
-                        on = False
-                        self.normaliser.factor.value = 0
-                    else:
-                        on = True
-                        self.normaliser.factor.value = (
-                            self.normaliser.default_brightness
+                if clk_state != clk_last_state:
+                    if dt_state != clk_state:
+                        self.normaliser.factor.value = min(
+                            self.normaliser.factor.value + self.step_size,
+                            self.max_brightness,
                         )
+                    else:
+                        self.normaliser.factor.value = max(
+                            self.normaliser.factor.value - self.step_size, 0.005
+                        )
+                clk_last_state = clk_state
 
-                sw_last_state = new_sw_state
-            sleep(0.001)
+                new_sw_state = GPIO.input(self.sw)
+                if new_sw_state != sw_last_state:
+                    if new_sw_state == 0:
+                        if on:
+                            on = False
+                            self.normaliser.factor.value = 0
+                        else:
+                            on = True
+                            self.normaliser.factor.value = (
+                                self.normaliser.default_brightness
+                            )
+
+                    sw_last_state = new_sw_state
+                sleep(0.001)
--- tests/test_hat.py
+++ tests/test_hat.py
@@ -32,7 +32,7 @@ class TestHat(TestCase):
                 (0, 0, 0),
                 (0, 0, 0),
                 (0, 0, 0),
-                (255, 0, 0),
+                (63, 0, 0),
             ],
         )
 
@@ -44,14 +44,14 @@ class TestHat(TestCase):
         self.assertEqual(  # noqa: PT009
             self.hat.lights[0:9],
             [
-                (0, 255, 0),
+                (0, 63, 0),
                 (0, 0, 0),
                 (0, 0, 0),
-                (0, 0, 255),
-                (0, 255, 0),
-                (0, 0, 255),
+                (0, 0, 63),
+                (0, 63, 0),
+                (0, 0, 63),
                 (0, 0, 0),
-                (0, 255, 0),
+                (0, 63, 0),
                 (0, 0, 0),
             ],
         )
@@ -79,7 +79,7 @@ class TestHat(TestCase):
         self.hat.sort("x")
         self.hat.illuminate(colours)
 
-        self.assertEqual(self.hat.lights[50], (255, 0, 0))  # noqa: PT009
-        self.assertEqual(self.hat.lights[12], (252, 0, 0))  # noqa: PT009
-        self.assertEqual(self.hat.lights[62], (218, 0, 0))  # noqa: PT009
-        self.assertEqual(self.hat.lights[36], (215, 0, 0))  # noqa: PT009
+        self.assertEqual(self.hat.lights[50], (63, 0, 0))  # noqa: PT009
+        self.assertEqual(self.hat.lights[12], (63, 0, 0))  # noqa: PT009
+        self.assertEqual(self.hat.lights[62], (54, 0, 0))  # noqa: PT009
+        self.assertEqual(self.hat.lights[36], (53, 0, 0))  # noqa: PT009
