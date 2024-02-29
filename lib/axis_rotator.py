0fa7d57e3e9bf6590b6ef959c7f61c7ecc4e1b01
pikesley <sam.pikesley@demandlogic.co.uk>
2024-02-29 18:48:20 +0000

    Vastly improved circliser

--- lib/axis_rotator.py
+++ lib/axis_rotator.py
@@ -1,52 +1,77 @@
-def circle(axes, interval=0.1):  # , direction="forwards"):
+from random import choice, randint
+
+
+# circle at a fixed altitude? I think this makes sense
+def circle(axes, interval=0.1, direction="forwards"):
     """Get a set of keys to go round and round."""
+    if direction == "backwards":
+        axes = list(axes)
+        axes.reverse()
+        axes = tuple(axes)
+
+    indeces = live_axes(axes)
     keys = [start_corner(axes)]
 
-    if "z" in axes:
-        count = 0 # do this as range I think
-        while count <= 1:
-            keys.append(
-                (keys[-1][0], keys[-1][1], round(keys[-1][2] + interval, 1))
-            )
-            count += interval
+    for operation in operations:
+        for index in indeces:
+            for _ in range(-10, 10, int(interval * 10)):
+                next_item = keys[-1].copy()
+                next_item[index] = operation(next_item[index], interval)
+                keys.append(next_item)
 
-    if "x" in axes:
-        count = 0
-        while count <= 1:
-            keys.append(
-                (round(keys[-1][0] + interval, 1), keys[-1][1], keys[-1][2])
-            )
-            count += interval
+    keys.pop()
 
-    if "z" in axes:
-        count = 1
-        while count >= 0:
-            keys.append(
-                (keys[-1][0], keys[-1][1], round(keys[-1][2] - interval, 1))
-            )
-            count -= interval
+    return [as_key(x) for x in keys]
 
-    if "x" in axes:
-        count = 1
-        while count > 0:
-            keys.append(
-                (round(keys[-1][0] - interval, 1), keys[-1][1], keys[-1][2])
-            )
-            count -= interval
 
-    return [f"sorts:{x}" for x in keys]
+def random_step(current_step, step_size=0.1):
+    """Get a (small) random step from where we are now."""
+    vals = [float(x) for x in current_step[7:-1].split(",")]
+    index = randint(0, 2)  # noqa: S311
+
+    target = vals[index]
+    if target == 1.0:
+        target = decrement(target, step_size)
+    elif target == -1.0:
+        target = increment(target, step_size)
+    else:
+        target = choice(operations)(target, step_size)  # noqa: S311
+
+    vals[index] = target
+    # TODO abstract this out
+    return as_key(vals)
+
+
+def increment(value, amount):
+    """Increment something."""
+    return round(value + amount, 1)
+
+
+def decrement(value, amount):
+    """Decrement something."""
+    return round(value - amount, 1)
+
+
+def live_axes(axes):
+    """Work out which axes are in play."""
+    return ["xyz".index(axis) for axis in axes]
 
 
 def start_corner(axes):
     """Work out the starting point."""
-    point = [0.0, 0.0, 0.0]
-    if "x" in axes:
-        point[0] = -1.0
+    point = []
+    for char in "xyz":
+        if char in axes:
+            point.append(-1.0)
+        else:
+            point.append(0.0)
 
-    if "y" in axes:
-        point[1] = -1.0
+    return point
 
-    if "z" in axes:
-        point[2] = -1.0
 
-    return tuple(point)
+def as_key(data):
+    """Turn a raw tuple into a Redis key."""
+    return f"sorts:{tuple(data)}"
+
+
+operations = [increment, decrement]
--- tests/test_axis_rotator.py
+++ tests/test_axis_rotator.py
@@ -1,9 +1,14 @@
-from lib.axis_rotator import circle, start_corner
+from lib.axis_rotator import circle, live_axes, start_corner
+
+
+def test_live_axes():
+    """Test it works out the axes in play."""
+    assert live_axes(("z", "x")) == [2, 0]
 
 
 def test_start_corner():
     """Test it works out the starting corner."""
-    assert start_corner(("z", "x")) == (-1.0, 0.0, -1.0)
+    assert start_corner(("z", "x")) == [-1.0, 0.0, -1.0]
 
 
 def test_simple_circle():
@@ -17,4 +22,54 @@ def test_simple_circle():
         "sorts:(1.0, 0.0, 0.0)",
         "sorts:(1.0, 0.0, -1.0)",  # back right
         "sorts:(0.0, 0.0, -1.0)",
+    ]
+
+
+def test_richer_circle():
+    """Test going round in a denser circle."""
+    assert circle(("z", "x"), interval=0.5) == [
+        "sorts:(-1.0, 0.0, -1.0)",  # back left
+        "sorts:(-1.0, 0.0, -0.5)",
+        "sorts:(-1.0, 0.0, 0.0)",
+        "sorts:(-1.0, 0.0, 0.5)",
+        "sorts:(-1.0, 0.0, 1.0)",  # front left
+        "sorts:(-0.5, 0.0, 1.0)",
+        "sorts:(0.0, 0.0, 1.0)",
+        "sorts:(0.5, 0.0, 1.0)",
+        "sorts:(1.0, 0.0, 1.0)",  # front right
+        "sorts:(1.0, 0.0, 0.5)",
+        "sorts:(1.0, 0.0, 0.0)",
+        "sorts:(1.0, 0.0, -0.5)",
+        "sorts:(1.0, 0.0, -1.0)",  # back right
+        "sorts:(0.5, 0.0, -1.0)",
+        "sorts:(0.0, 0.0, -1.0)",
+        "sorts:(-0.5, 0.0, -1.0)",
+    ]
+
+
+def test_reverse_circle():
+    """Test going round in a circle the other way."""
+    assert circle(("z", "x"), interval=1, direction="backwards") == [
+        "sorts:(-1.0, 0.0, -1.0)",
+        "sorts:(0.0, 0.0, -1.0)",
+        "sorts:(1.0, 0.0, -1.0)",
+        "sorts:(1.0, 0.0, 0.0)",
+        "sorts:(1.0, 0.0, 1.0)",
+        "sorts:(0.0, 0.0, 1.0)",
+        "sorts:(-1.0, 0.0, 1.0)",
+        "sorts:(-1.0, 0.0, 0.0)",
+    ]
+
+
+def test_y_z_circle():
+    """Test going round in a circle."""
+    assert circle(("y", "z"), interval=1) == [
+        "sorts:(0.0, -1.0, -1.0)",
+        "sorts:(0.0, 0.0, -1.0)",
+        "sorts:(0.0, 1.0, -1.0)",
+        "sorts:(0.0, 1.0, 0.0)",
+        "sorts:(0.0, 1.0, 1.0)",
+        "sorts:(0.0, 0.0, 1.0)",
+        "sorts:(0.0, -1.0, 1.0)",
+        "sorts:(0.0, -1.0, 0.0)",
     ]
