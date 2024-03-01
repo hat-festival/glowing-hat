fd88941bcee7b53b166446a13834cd22b5c2afea
pikesley <sam.pikesley@demandlogic.co.uk>
2024-03-01 21:06:50 +0000

    Use new APIs

--- lib/modes/cuttlefish.py
+++ lib/modes/cuttlefish.py
@@ -1,9 +1,8 @@
-import pickle
 from collections import deque
 
-from lib.axis_rotator import circle
-from lib.logger import logging
+from lib.axis_manager import AxisManager
 from lib.mode import Mode
+from lib.sorts_generator import SortsGenarator
 from lib.tools import hue_to_rgb
 
 
@@ -25,7 +24,12 @@ class Cuttlefish(Mode):
             self.jump = 0 - self.jump
 
         self.sort_hat()
-        self.axes = deque(circle(("x", "z")))
+
+        self.manager = AxisManager()
+
+        self.rotator = SortsGenarator(("x", "z"))
+        self.rotator.make_circle()
+        self.hat.pixels = self.manager.get_sort(self.rotator.next)
 
     def run(self):
         """Do the stuff."""
@@ -33,12 +37,11 @@ class Cuttlefish(Mode):
 
         count = 0
         while True:
-            self.hat.pixels = pickle.loads(self.redis.get(self.axes[0]))  # noqa: S301
             self.hat.illuminate(list(self.colours)[: self.hat.length])
             count += 1
-            if count == 3:  # noqa: PLR2004
-                logging.debug(self.axes[0])
-                self.axes.rotate()
+            # TODO move these numbers out to conf
+            if count == 1:
+                self.hat.pixels = self.manager.get_sort(self.rotator.next)
                 count = 0
 
             self.colours.rotate(self.jump)
