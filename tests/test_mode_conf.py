6ecef79bae2c45d81937595401649e7af924abc3
pikesley <sam.pikesley@demandlogic.co.uk>
2024-03-06 21:24:56 +0000

    Ensure conf is more consistent

---
+++ tests/test_mode_conf.py
@@ -0,0 +1,13 @@
+from lib.conf import conf
+from lib.custodian import Custodian
+from lib.hat import Hat
+from lib.modes_list import modes
+
+custodian = Custodian(namespace="test", conf=conf)
+custodian.populate(flush=True)
+hat = Hat()
+
+
+def test_conf_overlaps():
+    """Test we only have the conf we need."""
+    assert set(modes.keys()) == set(conf["modes"].keys())
