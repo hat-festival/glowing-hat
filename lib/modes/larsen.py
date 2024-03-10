43f1f01c5b1f90fe2c20cb224faf14c7bd756aa7
pikesley <sam.pikesley@demandlogic.co.uk>
2024-03-10 15:49:39 +0000

    Convincing Knight Rider

--- lib/modes/larsen.py
+++ lib/modes/larsen.py
@@ -1,5 +1,6 @@
 from collections import deque
 
+from lib.arrangements.larsen_list import LarsenList
 from lib.mode import Mode
 from lib.tools import scale_colour
 
@@ -7,40 +8,28 @@ from lib.tools import scale_colour
 class Larsen(Mode):
     """Larsen scanner."""
 
-    def __init__(self, hat, custodian):
-        """Construct."""
-        super().__init__(hat, custodian)
-        self.jump = self.data["jump"]
-        self.steps = self.data["steps"]
-        self.values = deque()
-
-        for i in range(self.steps):  # noqa: B007
-            self.values.append(0)
-        for i in range(self.steps):
-            self.values.append(i / self.steps)
-
-    def reconfigure(self):
+    def configure(self):
         """Configure ourself."""
-        if self.invert:
-            self.jump = 0 - self.jump
+        self.jump = self.data["jump"]
+        self.values = deque()
+        self.larsen_list = LarsenList(
+            len(self.hat),
+            head_width=self.data["head-width"],
+            tail_proportion=self.data["tail-proportion"],
+        )
 
-        self.sort_hat()
+        self.hat.sort("x")
 
     def run(self):
         """Do the stuff."""
-        self.reconfigure()
+        self.configure()
 
-        count = 0
-        clr = self.get_colour()
         while True:
-            rgbs = list(  # noqa: C417
-                map(lambda x: scale_colour(clr, x), list(self.values)[: len(self.hat)])
+            clr = self.get_colour()
+            self.from_list(
+                list(  # noqa: C417
+                    map(
+                        lambda x: scale_colour(clr, x), self.larsen_list.next(self.jump)
+                    )
+                )
             )
-            self.hat.illuminate(rgbs)
-            self.values.rotate(self.jump)
-
-            count += self.jump
-            if abs(count) >= len(self.values):
-                self.hat.reverse()
-                clr = self.get_colour()
-                count = 0
