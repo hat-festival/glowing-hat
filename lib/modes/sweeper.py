edc52749a5ad712afa7257ecb5c5157686eb7439
pikesley <sam.pikesley@demandlogic.co.uk>
2024-04-10 22:18:10 +0100

    This is much tidier

--- lib/modes/sweeper.py
+++ lib/modes/sweeper.py
@@ -1,5 +1,6 @@
 import pickle
 import random
+from collections import deque
 from pathlib import Path
 
 from lib.mode import Mode
@@ -13,12 +14,14 @@ class Sweeper(Mode):
         super().__init__(hat)
         self.max_laps = None
 
-        self.lap_types = {
-            "blank": {"show-blip": False, "side-effect": self.move_blip},
-            "entry": {"show-blip": False},
-            "regular": {"show-blip": True, "condition": self.should_move},
-            "exit": {"show-blip": True},
-        }
+        self.lap_types = deque(
+            [
+                {"name": "blank", "show-blip": False, "side-effect": self.move_blip},
+                {"name": "entry", "show-blip": False},
+                {"name": "regular", "show-blip": True, "condition": self.should_move},
+                {"name": "exit", "show-blip": True},
+            ]
+        )
 
     def configure(self):
         """Configure ourself."""
@@ -26,19 +29,17 @@ class Sweeper(Mode):
             Path("renders", "radar.pickle").read_bytes()
         )
         self.blip_index = None
-        self.fixtures = []
-        self.lap_type = "blank"
+        self.lap_type = self.lap_types[0]
 
     def run(self):
         """Do the work."""
         self.configure()
 
-        # lap_type = "blank"
         show_blip = False
         laps = 0
 
         while True:
-            show_blip = self.lap_types[self.lap_type]["show-blip"]
+            show_blip = self.lap_type["show-blip"]
 
             for count, values in enumerate(self.frames):
                 if count % self.conf["jump"] == 0:
@@ -46,7 +47,10 @@ class Sweeper(Mode):
                     self.hat.apply_hue(self.conf["hues"]["main"])
 
                     if self.blip_index:
-                        if self.lap_type == "entry" and values[self.blip_index] > 0.9:  # noqa: PLR2004
+                        if (
+                            self.lap_type["name"] == "entry"
+                            and values[self.blip_index] > 0.9  # noqa: PLR2004
+                        ):
                             show_blip = True
 
                         if show_blip:
@@ -54,11 +58,13 @@ class Sweeper(Mode):
                                 "blip"
                             ]
 
-                        if self.lap_type == "exit" and values[self.blip_index] < 0.1:  # noqa: PLR2004
+                        if (
+                            self.lap_type["name"] == "exit"
+                            and values[self.blip_index] < 0.1  # noqa: PLR2004
+                        ):
                             show_blip = False
 
                     self.hat.light_up()
-                    self.fixtures.append(list(self.hat.lights))
 
             if self.max_laps:
                 laps += 1
@@ -70,12 +76,12 @@ class Sweeper(Mode):
 
     def next_lap_type(self):
         """Determine the next `lap_type`."""
-        keys = list(self.lap_types.keys())
-        condition = self.lap_types[self.lap_type].get("condition", True)
-        side_effect = self.lap_types[self.lap_type].get("side-effect", None)
+        condition = self.lap_type.get("condition", True)
+        side_effect = self.lap_type.get("side-effect", None)
 
         if condition:
-            self.lap_type = keys[(keys.index(self.lap_type) + 1) % len(keys)]
+            self.lap_types.rotate(-1)
+            self.lap_type = self.lap_types[0]
 
         if side_effect:
             side_effect()
