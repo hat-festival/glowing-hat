79288360b0b75ddfc84b02a2d253c39fb279bdf7
pikesley <sam.pikesley@demandlogic.co.uk>
2024-04-10 22:09:25 +0100

    Clean up loads of guff

--- lib/modes/sweeper.py
+++ lib/modes/sweeper.py
@@ -4,8 +4,6 @@ from pathlib import Path
 
 from lib.mode import Mode
 
-# TODO: have a wandering axis_manager spot?
-
 
 class Sweeper(Mode):
     """Sweeper mode."""
@@ -15,6 +13,13 @@ class Sweeper(Mode):
         super().__init__(hat)
         self.max_laps = None
 
+        self.lap_types = {
+            "blank": {"show-blip": False, "side-effect": self.move_blip},
+            "entry": {"show-blip": False},
+            "regular": {"show-blip": True, "condition": self.should_move},
+            "exit": {"show-blip": True},
+        }
+
     def configure(self):
         """Configure ourself."""
         self.frames = pickle.loads(  # noqa: S301
@@ -22,20 +27,18 @@ class Sweeper(Mode):
         )
         self.blip_index = None
         self.fixtures = []
+        self.lap_type = "blank"
 
-    def run(self):  # noqa: C901, PLR0912
+    def run(self):
         """Do the work."""
         self.configure()
 
-        lap_type = "blank"
+        # lap_type = "blank"
         show_blip = False
         laps = 0
 
         while True:
-            if lap_type in ("regular", "exit"):
-                show_blip = True
-            if lap_type in ("blank", "entry"):
-                show_blip = False
+            show_blip = self.lap_types[self.lap_type]["show-blip"]
 
             for count, values in enumerate(self.frames):
                 if count % self.conf["jump"] == 0:
@@ -43,7 +46,7 @@ class Sweeper(Mode):
                     self.hat.apply_hue(self.conf["hues"]["main"])
 
                     if self.blip_index:
-                        if lap_type == "entry" and values[self.blip_index] > 0.9:  # noqa: PLR2004
+                        if self.lap_type == "entry" and values[self.blip_index] > 0.9:  # noqa: PLR2004
                             show_blip = True
 
                         if show_blip:
@@ -51,7 +54,7 @@ class Sweeper(Mode):
                                 "blip"
                             ]
 
-                        if lap_type == "exit" and values[self.blip_index] < 0.1:  # noqa: PLR2004
+                        if self.lap_type == "exit" and values[self.blip_index] < 0.1:  # noqa: PLR2004
                             show_blip = False
 
                     self.hat.light_up()
@@ -63,18 +66,24 @@ class Sweeper(Mode):
                 if laps == self.max_laps:
                     break
 
-            if lap_type == "entry":
-                lap_type = "regular"
+            self.next_lap_type()
 
-            elif (
-                lap_type == "regular"
-                and random.random() > self.conf["blip-move-threshold"]  # noqa: S311
-            ):
-                lap_type = "exit"
+    def next_lap_type(self):
+        """Determine the next `lap_type`."""
+        keys = list(self.lap_types.keys())
+        condition = self.lap_types[self.lap_type].get("condition", True)
+        side_effect = self.lap_types[self.lap_type].get("side-effect", None)
 
-            elif lap_type == "exit":
-                lap_type = "blank"
+        if condition:
+            self.lap_type = keys[(keys.index(self.lap_type) + 1) % len(keys)]
 
-            elif lap_type == "blank":
-                self.blip_index = random.randint(0, 99)  # noqa: S311
-                lap_type = "entry"
+        if side_effect:
+            side_effect()
+
+    def should_move(self):
+        """Determine if we should move the blip."""
+        return random.random() > self.conf["blip-move-threshold"]  # noqa: S311
+
+    def move_blip(self):
+        """Move the blip elsewhere."""
+        self.blip_index = random.randint(0, 99)  # noqa: S311
