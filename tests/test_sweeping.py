20b302d3a3c5345fc984039229d21e5a8c91e5f3
pikesley <sam.pikesley@demandlogic.co.uk>
2024-04-19 13:17:59 +0100

    Make test work

--- tests/test_sweeping.py
+++ tests/test_sweeping.py
@@ -1,33 +1,35 @@
-# from unittest.mock import patch
-# import random
-# import json
-# from pathlib import Path
-# import platform
-# from lib.hat import Hat
-# from lib.modes.sweeper import Sweeper
+import json
+import platform
+import random
+from pathlib import Path
+from unittest.mock import patch
+
+from lib.hat import Hat
+from lib.modes.sweeper import Sweeper
 
 
-# def test_sweeping():
-#     """Test it sweeps."""
-#     raw_fixture = json.loads(
-#         Path("tests/fixtures/sweeper/lights.json").read_text(encoding="utf-8")
-#     )
-#     fixture = []
-#     for item in raw_fixture:
-#         fixture.append([tuple(x) for x in item])
+def test_sweeping():
+    """Test it sweeps."""
+    raw_fixture = json.loads(
+        Path("tests/fixtures/sweeper/lights.json").read_text(encoding="utf-8")
+    )
+    fixture = []
+    for item in raw_fixture:
+        fixture.append([tuple(x) for x in item])  # noqa: PERF401
 
-#     with patch.object(random, "randint", return_value=33):
-#         with patch.object(random, "random", return_value=1.0):
-#             with patch.object(platform, "node", return_value="hatlights"):
-#                 hat = Hat()
-#                 hat.brightness_control.factor.value = 0.5
-#                 swp = Sweeper(hat)
-#                 swp.max_laps = 6
+    with patch.object(random, "randint", return_value=33):  # noqa: SIM117
+        with patch.object(random, "random", return_value=1.0):
+            with patch.object(platform, "node", return_value="hatlights"):
+                hat = Hat()
+                hat.brightness_control.factor.value = 0.5
+                swp = Sweeper(hat)
+                swp.max_laps = 6
 
-#                 swp.conf["hues"]["blip"] = 1.0
-#                 swp.conf["hues"]["main"] = 0.333333
-#                 swp.conf["jump"] = 4
+                swp.conf["hues"] = {}
+                swp.conf["hues"]["blip"] = 1.0
+                swp.conf["hues"]["main"] = 0.333333
+                swp.conf["jump"] = 4
 
-#                 swp.run()
+                swp.run()
 
-#                 assert hat.lights.record == fixture
+                assert hat.lights.record == fixture
