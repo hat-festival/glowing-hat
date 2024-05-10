b42752906af4732ea3e1a4995d58415552d5abf6
pikesley <sam.pikesley@demandlogic.co.uk>
2024-05-10 22:32:31 +0100

    Nuke redundant nonsense
    We have a much better solution now

--- make_words/create.sh
+++
@@ -1,6 +0,0 @@
-OUTFILE=${1}
-shift
-
-docker run --volume $(pwd)/../conf/panel/strings/:/opt/output/ word-maker ${OUTFILE} "${@}"
-
-echo "# ${@}" >> $(pwd)/../conf/panel/strings/${OUTFILE}.yaml
--- make_words/script.sh
+++
@@ -1,4 +0,0 @@
-OUTFILE=${1}
-shift
-
-ruby -e "require 'nineteen/eighty/two' ; require 'yaml' ; File.write('/opt/output/${OUTFILE}.yaml', YAML.dump(Nineteen::Eighty::Two::Spectrum['${@}  ']))"
